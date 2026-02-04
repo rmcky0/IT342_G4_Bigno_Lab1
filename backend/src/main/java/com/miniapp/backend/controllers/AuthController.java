4adf1d12548b341246ed0e371c50688a589ab24d
Richemmae V. Bigno <richem.miccah.bigno@gmail.com>
2026-02-04 21:03:52 +0800

    feat(backend): added cors origin access to 3000

--- backend/src/main/java/com/miniapp/backend/controllers/AuthController.java
+++ backend/src/main/java/com/miniapp/backend/controllers/AuthController.java
@@ -1,17 +1,23 @@
 package com.miniapp.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.miniapp.backend.models.User;
 import com.miniapp.backend.services.AuthService;
 import com.miniapp.backend.services.AuthService.LoginRequest;
 import com.miniapp.backend.services.AuthService.RegisterRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AuthController {
     
     @Autowired
