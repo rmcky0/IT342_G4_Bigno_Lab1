294876eb70f6178f6d1ae15700bfa970c49537e1
Richemmae V. Bigno <richemmae.bigno@cit.edu>
2026-02-11 16:45:58 +0800

    feat(mobile): updated setup and added login activity

---
+++ mobile/app/src/main/java/com/example/miniapp/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.miniapp.ui
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.miniapp.R
+
+class LoginActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
