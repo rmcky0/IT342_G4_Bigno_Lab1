2eea5a8ac6d5d4b2c2845046f6aec1b4701419fc
Richemmae V. Bigno <richem.miccah.bigno@gmail.com>
2026-02-11 11:19:48 +0800

    chore(frontend): remove Login/Register aliases; use AuthPage directly

--- web/src/App.js
+++ web/src/App.js
@@ -7,8 +7,7 @@ import {
 } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import AuthPage from "./pages/AuthPage";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
@@ -17,8 +16,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<AuthPage />} />
+          <Route path="/login" element={<AuthPage />} />
           <Route
             path="/dashboard"
             element={
